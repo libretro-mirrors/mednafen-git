1eea3360beb21c8424290720c369c6b4663769ee
Petr Semiletov <tea@list.ru>
2017-04-09 15:20:49 +0000

    added missing source files from mednafen tarball

---
+++ src/string/string.h
@@ -0,0 +1,42 @@
+/******************************************************************************/
+/* Mednafen - Multi-system Emulator                                           */
+/******************************************************************************/
+/* string.h:
+**  Copyright (C) 2007-2017 Mednafen Team
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __MDFN_STRING_STRING_H
+#define __MDFN_STRING_STRING_H
+
+// Removes whitespace from the beginning of the string.
+void MDFN_ltrim(char* s);
+void MDFN_ltrim(std::string& s);
+
+// Removes whitespace from the end of the string.
+void MDFN_rtrim(char* s);
+void MDFN_rtrim(std::string& s);
+
+// Removes whitespace from the beginning and end of the string.
+void MDFN_trim(char* s);
+void MDFN_trim(std::string& s);
+
+// Replaces control characters with space(' ') character.
+void MDFN_zapctrlchars(char* s);
+void MDFN_zapctrlchars(std::string& s);
+
+std::vector<std::string> MDFN_strsplit(const std::string& str, const std::string& delim = ",");
+#endif
